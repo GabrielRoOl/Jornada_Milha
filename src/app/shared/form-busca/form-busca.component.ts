f13443ad317c7bad94aa2bbf9f02391b76327b07
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-12-04 11:17:01 -0300

    Projeto finalizado

--- src/app/shared/form-busca/form-busca.component.ts
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ModalComponent } from '../modal/modal.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-form-busca',
   templateUrl: './form-busca.component.html',
-  styleUrls: ['./form-busca.component.scss']
+  styleUrls: ['./form-busca.component.scss'],
 })
 export class FormBuscaComponent {
+  constructor(public dialog: MatDialog) {}
 
+  openDialog() {
+    this.dialog.open(ModalComponent);
+  }
 }
